cb061066a6ebf593f5e6ec5c94b210129ac6d5e0
dccrazyboy <dccrazyboy@gmail.com>
2013-01-29 19:57:19 +0800

    完成，可以查询多门课程

--- query.py
+++ query.py
@@ -2,7 +2,6 @@
 # coding:utf-8
 import requests
 import re
-
 # http://210.27.12.1:90/queryDegreeScoreAction.do?studentid=xdleess20120514sn1585&degreecourseno=0821005
 
 def getScore(url,stu,course):
@@ -20,14 +19,39 @@ def getScore(url,stu,course):
 		start = tbody.rfind('>',0,end)
 		val.append(tbody[start+1:end].strip())
 		tbody = tbody[end+len('</td>'):]
-	
 	return val
+
+def getAllScore(url,stu,courses):
+	scores = []
+	for course in courses:
+		scores.append(getScore(url,stu,course))
+	return scores
+
 
 if __name__ == '__main__':
 	url = 'http://210.27.12.1:90/queryDegreeScoreAction.do'
 	stu = 'xdleess20120514sn1585'
-	course = '0321022'
-	for s in getScore(url,stu,course):
-		print s,
-
- 
+	courses = [
+		'0022001',
+		'0321003',
+		'0321007',
+		'0321011',
+		'0321020',
+		'0321022',
+		'0322102',
+		'0821001',
+		'0821100',
+		'0821101',
+		'0022002',
+		'0022004',
+		'0321006',
+		'0322208',
+		'0821001',
+		'0821005',
+		'0821102',
+		'0821004',
+		]
+	scores = getAllScore(url,stu,courses)
+	print '课程名称,学分,成绩'
+	for each in scores:
+		print "%s,%s,%s" % (each[1], each[2], each[4])
